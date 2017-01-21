250aada0d73450466d81b54062a5a89df3db16c7
stutivish <stuti.vish@gmail.com>
2017-01-20 19:40:00 -0500

    resolving merge conflict in index.js

--- routes/index.js
+++ routes/index.js
@@ -64,7 +64,10 @@ router.get('/logout', function(req, res, next) {
   res.redirect('/');
 });
 
+//Attempt to incorporate options.limit parameter
+
 /* Incorporating civi API through node package civicrm */
+// <<<<<<< Updated upstream
 
 // TEST: Getting first 25 contacts: name and email
 // crmAPI.get('contact', {contact_type:'Individual', return:'display_name, street_address'},
@@ -77,24 +80,51 @@ router.get('/logout', function(req, res, next) {
 // );
 
 // GET new emergency requests: Name, Location, Phone Number, Details
-crmAPI.get('Activity', {activity_type_id:'Emergency Food Package', status_id:'Scheduled', options:{limit:3}, return:'custom_102,location,phone_number,details'},
+// crmAPI.get('Activity', {activity_type_id:'Emergency Food Package', status_id:'Scheduled', options:{limit:3}, return:'custom_102,location,phone_number,details'},
+//   function (result) {
+//     for (var i in result.values) {
+//       val = result.values[i];
+//       console.log(val.id + ": " + val.custom_102 + " " + val.location + " " + val.phone_number + " " + val.details);
+//     }
+//   }
+// );
+
+crmAPI.create('contact', {id:'12966', return:'display_name,gender_id'},
   function (result) {
-    for (var i in result.values) {
-      val = result.values[i];
-      console.log(val.id + ": " + val.custom_102 + " " + val.location + " " + val.phone_number + " " + val.details);
-    }
+    //console.log(result); 
+    val=result.values[0]; 
+    val.gender_id='2'; 
+    console.log(result); 
+    console.log('UPDATED CONTACT: '+ val.display_name + " " + val.gender_id);
+
+    crmAPI.get('contact', {tag:'190', return:'display_name,phone,country,gender_id'}, 
+      function (result){
+        console.log(result); 
+        for (var i in result.values) {
+          val = result.values[i];
+          console.log(val.id + ": " + val.display_name + " " + val.phone + " " + val.country + " " + val.gender_id);
+        }
+    });
+
   }
 );
 
 // GET volunteers tagged with 'Emergency Food Package Volunteer': Name, Phone Number
-crmAPI.get('contact', {tag:'190', return:'display_name,phone'},
-  function (result) {
-    for (var i in result.values) {
-      val = result.values[i];
-      console.log(val.id + ": " + val.display_name + " " + val.phone);
-    }
-  }
-);
+// crmAPI.get('contact', {tag:'190', return:'display_name,phone,country'},
+//   function (result) {
+//     for (var i in result.values) {
+//       val = result.values[i];
+//       console.log(val.id + ": " + val.display_name + " " + val.phone + " " + val.country);
+// =======
+// crmAPI.get('contact', {contact_type:'Individual', return:'display_name, street_address, phone'},
+//   function (result) {
+//     for (var i in result.values) {
+//       val = result.values[i];
+//       console.log(val.id + ": " + val.display_name + " " + val.street_address + " " + val.phone);
+// >>>>>>> Stashed changes
+//     }
+//   }
+// );
 
 
 module.exports = router;
