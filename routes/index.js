18c281cc5eb2cc2e0772a05f3f1da43b06a52ccd
Kristy Carpenter <kcarp@mit.edu>
2017-01-27 19:35:10 -0500

    receiving sms test

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bcrypt = require('bcrypt');
 var router = express.Router();
 
+//for receiving sms
+var bodyParser = require('body-parser');
+var app = express();
+
 var User = require('../models/user');
 var Admin = require('../models/admin');
 var Volunteer = require('../models/volunteer');
@@ -266,5 +270,23 @@ function newAdmins() {
     }
   );
 }
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('port', (process.env.PORT || 5000));
+app.all('/receive_sms/', function(request, response) {
+    // Sender's phone number
+    var from_number = request.body.From || request.query.From;
+    // Receiver's phone number - Plivo number
+    var to_number = request.body.To || request.query.To;
+    // The text which was received
+    var text = request.body.Text || request.query.Text;
+    console.log('Message received - From: ', from_number, ', To: ', to_number, ', Text: ', text);
+    response.send("Message received");
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
 
 module.exports = router;
